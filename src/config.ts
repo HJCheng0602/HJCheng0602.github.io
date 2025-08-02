8714d116f8882cab65981b976f78fd0d60bf0397
hjcer <3194896837@qq.com>
2025-08-02 10:38:48 +0800

    updateheadphoto

--- src/config.ts
+++ src/config.ts
@@ -52,7 +52,7 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	avatar: "assets/images/headphoto.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 	name: "Han Jincheng",
 	bio: "Nothing",
 	links: [
