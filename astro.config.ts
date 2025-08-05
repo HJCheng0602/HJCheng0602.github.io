9b6f85e5fe6feabd2af50663bfd10280ec2357ee
hjcer <3194896837@qq.com>
2025-08-05 20:48:14 +0800

    repair4

--- astro.config.ts
+++ astro.config.ts
@@ -25,8 +25,8 @@ import config from './src/site.config.ts'
 // https://astro.build/config
 export default defineConfig({
   // Top-Level Options
-  site: 'https://hjcheng0602.github.io/',
-  base: '/hanjincheng.github.io/',
+  site: 'https://hjcheng0602.github.io',
+  base: 'personalsite',
   trailingSlash: 'never',
 
   // Adapter
